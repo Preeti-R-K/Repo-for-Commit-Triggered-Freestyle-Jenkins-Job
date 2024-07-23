3709d57e2b6c830db50d33987fd323a48f07bf1e
Preeti-R-K <52197806+Preeti-R-K@users.noreply.github.com>
2024-07-23 16:50:24 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -11,7 +11,7 @@ public class Pattern {
         for (i = 1; i <= n; i++) {
             // inner loop to print space
             for (j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             // inner loop to print star
             for (j = 1; j <= i; j++) {
